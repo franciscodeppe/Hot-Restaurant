89556cdb15eba7ba8397095dd64c63d2782737f9
francisco <francisco.deppe@gmail.com>
2017-07-27 19:05:30 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,86 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Star Wars Characters (DATA)
+// =============================================================
+var characters = [{
+  routeName: "wolverine",
+  name: "Wolverine",
+  type: "Jedi Master",
+  age: 145,
+  Group: "Xmen"
+}, {
+  routeName: "deadpool",
+  name: "DeadPool",
+  type: "Mutant",
+  age: 40,
+  Group: "NA"
+}, {
+  routeName: "Spiderman",
+  name: "Spiderman",
+  type: "Mutant",
+  age: 22,
+  Group: "Avengers"
+}];
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "view.html"));
+});
+
+app.get("/add", function(req, res) {
+  res.sendFile(path.join(__dirname, "add.html"));
+
+});
+
+// Search for Specific Character (or all characters) - provides JSON
+app.get("/api/:characters?", function(req, res) {
+  var chosen = req.params.characters;
+
+  if (chosen) {
+    console.log(chosen);
+
+    for (var i = 0; i < characters.length; i++) {
+      if (chosen === characters[i].routeName) {
+        return res.json(characters[i]);
+      }
+    }
+    return res.json(false);
+  }
+  return res.json(characters);
+});
+
+// Create New Characters - takes in JSON input
+app.post("/api/new", function(req, res) {
+  var newcharacter = req.body;
+  newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newcharacter);
+
+  characters.push(newcharacter);
+
+  res.json(newcharacter);
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
